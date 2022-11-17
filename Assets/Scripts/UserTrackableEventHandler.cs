752d1cf5b4c8564601733b38672a86ac763389da
trainman1974trainman1974 <102028173+trainman1974trainman1974@users.noreply.github.com>
2022-11-17 21:30:16 +0300

    тест

---
+++ Assets/Scripts/UserTrackableEventHandler.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserTrackableEventHandler : DefaultObserverEventHandler
+{
+    /*
+    public GameObject _vPlayer;
+
+    protected override void OnTrackingFound()
+    {
+        //_vPlayer.SetActive (true);
+
+        //videoPlayer _vPlayer = videoPlayer.FindGameObjectWithTag("videoPlayer");
+        //videoPlayer = videoPlayer.FindGameObjectWithTag("videoPlayer");
+        //videoPlayer.Play();
+        
+        GameObject horse = GameObject.FindGameObjectWithTag("Horse");
+        horse.transform.SetParent(this.gameObject.transform, false);
+        base.OnTrackingFound();     
+        
+    }
+
+    public void videoPlayerPlay()
+    {
+        _vPlayer.SetActive(true);
+    }
+    */
+}
