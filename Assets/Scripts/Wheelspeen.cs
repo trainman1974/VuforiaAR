b8b4f71b4d4c5a2649df975c059047aefeea366e
MARXstoodin <abramovarsenii45@gmail.com>
2022-11-20 14:00:19 +0300

    Optimisation and Corutine

--- Assets/Scripts/Wheelspeen.cs
+++ Assets/Scripts/Wheelspeen.cs
@@ -8,7 +8,7 @@ public class Wheelspeen : MonoBehaviour
     public GameObject Wheel;
     public GameObject sparks;
     public GameObject sparks1;
-    bool IsSpining=true;
+    bool IsSpining=false;
     float speed=0f;
     private Renderer RendererSparks;
     private Renderer RendererSparks1;
@@ -18,10 +18,10 @@ public class Wheelspeen : MonoBehaviour
     {
         RendererSparks = sparks.GetComponent<Renderer>();
         RendererSparks1 = sparks1.GetComponent<Renderer>();
+        StartCoroutine(Fade(RendererSparks, RendererSparks1, alpha, mainTexture));
     }
     void Update()
     {
-        //sparks = Color.Lerp(sparks.linear, new Vector4(sparks.linear.r, sparks.linear.g, sparks.linear.b, 0), 0.001f);
         if (!IsSpining && speed<=0)
         {
             speed += 0.0002f;
@@ -32,14 +32,24 @@ public class Wheelspeen : MonoBehaviour
             speed -= 0.0002f;
             alpha -= 0.0005f;
         }
+        if (!IsSpining && speed <= 0 || IsSpining && speed > -0.4f)
+        {
+            StartCoroutine(Fade(RendererSparks, RendererSparks1, alpha, mainTexture));
+        }
         updatee += Time.deltaTime;
         if (updatee > 0.01f)
         {
             updatee = 0.0f;
             Wheel.transform.Rotate(0, 0, speed);
         }
+    }
+    public void WheelStop()
+    {
+        IsSpining = !IsSpining;
+    }
+    IEnumerator Fade(Renderer RendererSparks, Renderer RendererSparks1, float alpha, Texture2D mainTexture)
+    {
         Texture2D _NewTexture = new Texture2D(32, 32);
-
         Vector4 _Color = new Vector4(alpha, alpha, alpha, alpha);
         for (int y = 0; y < 32; y++)
         {
@@ -51,39 +61,7 @@ public class Wheelspeen : MonoBehaviour
         }
         _NewTexture.Apply();
         RendererSparks.material.mainTexture = _NewTexture;
-    }
-    public void WheelStop()
-    {
-        IsSpining = !IsSpining;
-        //Wheel.transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0,0,100), updatee);
-        if (!IsSpining)
-        {
-            //RendererSparks.material.mainTexture = testTexture;            
-            //Color c = RendererSparks1.material.color;
-
-            /// рабочие значения от 0 до 1
-            /// придумай как менять эти значения.
-
-            ////создание текстуры
-            Texture2D _NewTexture = new Texture2D(32, 32);
-
-            Vector4 _Color = new Vector4 (alpha,alpha,alpha,alpha);
-            for (int y = 0; y < 32; y++)
-            {
-             for (int x = 0; x < 32; x++)
-             { 
-                Vector4 _OldColor = mainTexture.GetPixel(x,y);
-                _NewTexture.SetPixel(x, y, (_OldColor - _Color)); 
-             }             
-            }     
-            _NewTexture.Apply();
-            RendererSparks.material.mainTexture = _NewTexture;
-
-        }
-        else
-        {
-            RendererSparks.material.mainTexture = mainTexture;
-            RendererSparks1.material.mainTexture = mainTexture;
-        }
+        RendererSparks1.material.mainTexture = _NewTexture;
+        yield return null;
     }
 }
